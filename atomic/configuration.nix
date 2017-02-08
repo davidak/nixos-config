b7b8056e8c6ccadacad918ea94833e61c0b50052
davidak <git@davidak.de>
2017-02-08 02:17:35 +0100

    atomic: use PHP 7.0 and disable opcache

--- atomic/configuration.nix
+++ atomic/configuration.nix
@@ -113,22 +113,23 @@ in
 
   # PHP-FPM
   services.phpfpm = {
-    phpPackage = pkgs.php56;
+    #phpPackage = pkgs.php56;
     phpOptions = ''
       date.timezone = "Europe/Berlin"
       ;memory_limit = 256M
       ;max_execution_time = 60
 
-      zend_extension = ${pkgs.php56}/lib/php/extensions/opcache.so
-      opcache.enable = 1
-      opcache.memory_consumption = 64
-      opcache.interned_strings_buffer = 16
-      opcache.max_accelerated_files = 10000
-      opcache.max_wasted_percentage = 5
-      opcache.use_cwd = 1
-      opcache.validate_timestamps = 1
-      opcache.revalidate_freq = 2
-      opcache.fast_shutdown = 1
+      # Problems with Caddy https://github.com/mholt/caddy/issues/1204
+      #zend_extension = ${pkgs.php56}/lib/php/extensions/opcache.so
+      #opcache.enable = 0
+      #opcache.memory_consumption = 64
+      #opcache.interned_strings_buffer = 16
+      #opcache.max_accelerated_files = 10000
+      #opcache.max_wasted_percentage = 5
+      #opcache.use_cwd = 1
+      #opcache.validate_timestamps = 1
+      #opcache.revalidate_freq = 2
+      #opcache.fast_shutdown = 1
     '';
     pools = {
       piwik = {
