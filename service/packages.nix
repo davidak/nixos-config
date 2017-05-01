39049f6b342d8a4af8750553c7666d1cd4ac272e
davidak <git@davidak.de>
2017-05-01 22:22:44 +0200

    add more default packages

--- service/packages.nix
+++ service/packages.nix
@@ -3,16 +3,22 @@
 {
   environment.systemPackages = with pkgs; [
     htop
+    iotop
     wget
     curl
+    tcpdump
     telnet
     whois
+    iperf
     file
+    lsof
+    strace
     unzip
     rsync
     screen
     mailutils
     tree
+    jq
     gitAndTools.gitFull
   ];
 }
