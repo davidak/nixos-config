3a9f9f196a2446d2d00908bc2624501d2fab87b6
davidak <git@davidak.de>
2017-05-05 01:41:52 +0200

    add nas config

---
+++ nas/README.md
@@ -0,0 +1,14 @@
+NixOS NAS
+=========
+
+Why should you buy an overpriced NAS or install a NAS distribution like [FreeNAS](https://freenas.org/) or [Rockstor](http://rockstor.com/) when you can setup Samba with few lines of code in NixOS?
+
+Projekt still **WORK IN PROGRESS**!
+
+Features
+--------
+
+- SMB Shares
+
+Install
+-------
---
+++ nas/configuration.nix
@@ -0,0 +1,63 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      /etc/nixos/hardware-configuration.nix
+    ];
+
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    device = "/dev/sda";
+  };
+
+  networking = {
+#    interfaces = {
+#      enp0s18.ip4 = [ { address = "10.0.0.252"; prefixLength = 8; } ];
+#    };
+
+#    nameservers = [ "10.0.0.1" "8.8.8.8" ];
+#    defaultGateway = "10.0.0.1";
+
+#    useDHCP = false;
+    useDHCP = true;
+  };
+
+  time.timeZone = "Europe/Berlin";
+  i18n = {
+    consoleKeyMap = "de";
+    defaultLocale = "de_DE.UTF-8";
+  };
+
+  services.samba = {
+    enable = true;
+    extraConfig = ''
+      [backup]
+      path = /data/backup
+      read only = no
+      guest ok = no
+
+      [bilder]
+      path = /data/bilder
+      read only = no
+      guest ok = yes
+
+      [musik]
+      path = /data/musik
+      read only = no
+      guest ok = yes
+
+      [videos]
+      path = /data/videos
+      read only = no
+      guest ok = yes
+    '';
+  };
+
+  services.samba.invalidUsers = [];
+
+  services.openssh.enable = true;
+  services.openssh.permitRootLogin = "yes";
+  users.extraUsers.root.openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC0gSy7qdULWLSpGuGM6BAoFztX123g/cbW6x3TfzKo0s59y9OrzHrCSTYg3QN9BY1jLRp5DSMjHvsPS1Z/yp3EIJJS/dDso5/noDqOMBLOQgIdCLKipTudngpFDvnCAAg0IQl6iuVRznQvq9Xww65uYyR3OAv4DMvHFQn0qa5G3ZHCoj7I6FATTwGDKPeuqVF2MtdXC1XXx7v7zsar1sBhibUlbWSWhSvw+vhM+Qtj95wkHzI8O93Xy8Vqb5/OoXQDGyA0MnORCLeE8t7EvUi9ukXGz6QMwRX/T1RTLBP+pvrT5UyPtchzgZigbxvegnAy8HRA7I9TlUSFnTVvN6sg6z7n/F09HX1ETBv1qce/uuDc+npfM6Kdykz93ydro1ZfnPabD6rvie972EK5IVsO6n5066vVVhUt9QxDl2CDa0tLBxnGovvV1nmtcjq2AewOX2vj5qD0U256AiiS8tNA0i9GQLW90x6o1/Ih2xaPagfrRmpQjR1ecbEFYxT34Lp5ZuC9x5Nm67RGb4JvvbMrz3qjR5YARKOiryJ5owrN3TUJmYp75xT7QBGkXBwhQJZwwBFhg5rKC5BJIj5x4PGJXrwHHuk6gpbLRbgoT69NmJYIkKZaPSIt+oOzVmgKBM5LTtI4JI8kPs2CHo2FwuYAnP9XAfGoTuB/Ir9ECkFoEQ== davidak" ];
+}
