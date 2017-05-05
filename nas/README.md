09028390d9ec99bbc5d044a762c8562602d00530
davidak <git@davidak.de>
2017-05-05 02:35:27 +0200

    nas: add basic config, monitoring and user

--- nas/README.md
+++ nas/README.md
@@ -9,6 +9,22 @@ Features
 --------
 
 - SMB Shares
+- Monitoring with [netdata](https://my-netdata.io/) and [vnstat](http://humdi.net/vnstat/)
 
 Install
 -------
+
+```
+
+# sync config to maschine
+imac:code davidak$ rsync -ahz --progress nixos root@10.0.2.82:
+# symlink config
+[root@nixos:~]# ln -s /root/nixos/nas/configuration.nix /etc/nixos/configuration.nix
+# configure system
+[root@nixos:~]# nixos-rebuild switch
+reboot
+# set password
+[root@nas:~]# passwd davidak
+# create smb user, set password
+smbpasswd -a davidak
+```
--- nas/configuration.nix
+++ nas/configuration.nix
@@ -1,9 +1,18 @@
 { config, pkgs, ... }:
 
+let
+  pubkey = import ../service/pubkey.nix;
+in
 {
   imports =
     [
       /etc/nixos/hardware-configuration.nix
+      ../service/packages.nix
+      ../service/ssh.nix
+      ../service/fail2ban.nix
+      ../service/postfix.nix
+      ../service/ntp.nix
+      ../service/vim.nix
     ];
 
   boot.loader.grub = {
@@ -12,13 +21,37 @@
     device = "/dev/sda";
   };
 
-  networking = {
+  networking = rec {
+    hostName = "NAS";
+    domain = "lan";
+
+    # fix for missing hosts entry https://github.com/NixOS/nixpkgs/issues/1248
+    extraHosts = ''
+      127.0.0.1 localhost.localdomain localhost
+      172.31.1.100 ${hostName}.${domain} ${hostName}
+
+      # The following lines are desirable for IPv6 capable hosts
+      ::1     ip6-localhost ip6-loopback
+      fe00::0 ip6-localnet
+      ff00::0 ip6-mcastprefix
+      ff02::1 ip6-allnodes
+      ff02::2 ip6-allrouters
+      ff02::3 ip6-allhosts
+    '';
+
 #    interfaces = {
 #      enp0s18.ip4 = [ { address = "10.0.0.252"; prefixLength = 8; } ];
 #    };
 
 #    nameservers = [ "10.0.0.1" "8.8.8.8" ];
 #    defaultGateway = "10.0.0.1";
+
+    firewall = {
+      enable = false;
+      allowPing = true;
+      allowedTCPPorts = [ 80 443 8384 19999 ];
+      allowedUDPPorts = [];
+    };
 
 #    useDHCP = false;
     useDHCP = true;
@@ -30,8 +63,14 @@
     defaultLocale = "de_DE.UTF-8";
   };
 
+  # Monitoring
+  services.netdata.enable = true;
+  services.vnstat.enable = true;
+
+  # SMB Shares
   services.samba = {
     enable = true;
+    syncPasswordsByPam = true;
     extraConfig = ''
       [backup]
       path = /data/backup
@@ -55,9 +94,15 @@
     '';
   };
 
-  services.samba.invalidUsers = [];
+  users.extraUsers.davidak = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [ pubkey.davidak ];
+  };
 
-  services.openssh.enable = true;
-  services.openssh.permitRootLogin = "yes";
-  users.extraUsers.root.openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC0gSy7qdULWLSpGuGM6BAoFztX123g/cbW6x3TfzKo0s59y9OrzHrCSTYg3QN9BY1jLRp5DSMjHvsPS1Z/yp3EIJJS/dDso5/noDqOMBLOQgIdCLKipTudngpFDvnCAAg0IQl6iuVRznQvq9Xww65uYyR3OAv4DMvHFQn0qa5G3ZHCoj7I6FATTwGDKPeuqVF2MtdXC1XXx7v7zsar1sBhibUlbWSWhSvw+vhM+Qtj95wkHzI8O93Xy8Vqb5/OoXQDGyA0MnORCLeE8t7EvUi9ukXGz6QMwRX/T1RTLBP+pvrT5UyPtchzgZigbxvegnAy8HRA7I9TlUSFnTVvN6sg6z7n/F09HX1ETBv1qce/uuDc+npfM6Kdykz93ydro1ZfnPabD6rvie972EK5IVsO6n5066vVVhUt9QxDl2CDa0tLBxnGovvV1nmtcjq2AewOX2vj5qD0U256AiiS8tNA0i9GQLW90x6o1/Ih2xaPagfrRmpQjR1ecbEFYxT34Lp5ZuC9x5Nm67RGb4JvvbMrz3qjR5YARKOiryJ5owrN3TUJmYp75xT7QBGkXBwhQJZwwBFhg5rKC5BJIj5x4PGJXrwHHuk6gpbLRbgoT69NmJYIkKZaPSIt+oOzVmgKBM5LTtI4JI8kPs2CHo2FwuYAnP9XAfGoTuB/Ir9ECkFoEQ== davidak" ];
+  # Packages
+  environment.systemPackages = with pkgs; [ vnstat samba ];
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "17.03";
 }
