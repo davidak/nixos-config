eebc282113a97d54616d309efaf8be79a96c2040
davidak <git@davidak.de>
2017-08-22 23:18:01 +0200

    atomic: clean up workaround
    issue is fixed

--- atomic/configuration.nix
+++ atomic/configuration.nix
@@ -36,20 +36,6 @@ in
     hostName = "atomic";
     domain = "davidak.de";
 
-    # fix for missing hosts entry https://github.com/NixOS/nixpkgs/issues/1248
-    extraHosts = ''
-    127.0.0.1 localhost.localdomain localhost
-    172.31.1.100 ${hostName}.${domain} ${hostName}
-
-    # The following lines are desirable for IPv6 capable hosts
-    ::1     ip6-localhost ip6-loopback
-    fe00::0 ip6-localnet
-    ff00::0 ip6-mcastprefix
-    ff02::1 ip6-allnodes
-    ff02::2 ip6-allrouters
-    ff02::3 ip6-allhosts
-    '';
-
     interfaces = {
       enp0s3.ip4 = [
         # external 138.201.246.37
