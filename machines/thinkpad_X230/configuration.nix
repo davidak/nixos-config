c7929ac884831579f07ac0eee1629d5a20f1c626
davidak <git@davidak.de>
2017-12-18 07:13:50 +0100

    update X230

--- machines/thinkpad_X230/configuration.nix
+++ machines/thinkpad_X230/configuration.nix
@@ -4,20 +4,28 @@
   imports =
     [
       /etc/nixos/hardware-configuration.nix
+      ../../profiles/hardware.nix
       ../../profiles/notebook.nix
       ./x230.nix
       #../../services/bluetooth.nix
       ../../profiles/desktop.nix
+      ../../profiles/communication.nix
     ];
 
   boot.loader.grub.device = "/dev/sda";
-
-  /*# disk encryption
   boot.initrd.luks.devices = [
     { name = "rootfs";
-      device = "/dev/sda2";
-      preLVM = true; }
-  ];*/
+      device = "/dev/disk/by-uuid/3d49cc3e-7771-4116-ac1e-abf4ef0400d0";
+      preLVM = true;
+      allowDiscards = true;
+    }
+  ];
+
+  # no access time and continuous TRIM for SSD
+  fileSystems."/".options = [ "noatime" "discard" ];
+  fileSystems."/boot".options = [ "noatime" "discard" ];
+
+  hardware.cpu.intel.updateMicrocode = true;
 
   networking = {
     hostName = "X230";
