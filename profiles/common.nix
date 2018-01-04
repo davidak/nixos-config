6d82dac2c393ed91aede13d937502b0e3b455a5b
davidak <git@davidak.de>
2018-01-04 23:32:25 +0100

    add fix for Meltdown and Spectre vuln.

--- profiles/common.nix
+++ profiles/common.nix
@@ -11,6 +11,10 @@
       ../services/localization.nix
     ];
 
+  # use latest kernel to fix Meltdown and Spectre vuln.
+  # https://github.com/NixOS/nixpkgs/issues/33414
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   # install basic packages
   environment.systemPackages = with pkgs; [
     htop
