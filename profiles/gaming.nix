3a377ab92b5ace27bcbbfe24b97d7f6fa1b50d1c
davidak <git@davidak.de>
2018-02-06 19:42:23 +0100

    add gaming profile

---
+++ profiles/gaming.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  # required for steam https://nixos.wiki/wiki/Steam
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
+  nixpkgs.config.allowUnfree = true;
+
+  # install packages
+  environment.systemPackages = with pkgs; [
+    steam
+    steam-run
+  ];
+}
