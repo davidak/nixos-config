d48b689ebabc06a50b0d7e0a61bf962021e28415
davidak <git@davidak.de>
2018-06-01 08:02:26 +0200

    enable kdenlive again

--- profiles/video-production.nix
+++ profiles/video-production.nix
@@ -7,8 +7,8 @@
     obs-studio
     # https://github.com/NixOS/nixpkgs/issues/20449
     #pitivi
-    # https://github.com/NixOS/nixpkgs/issues/29614
-    #kdeApplications.kdenlive
+    # partly broken https://github.com/NixOS/nixpkgs/issues/29614
+    kdeApplications.kdenlive
     # too simple, also broken
     #openshot-qt
   ];
