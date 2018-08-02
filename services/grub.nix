d2d343bdd73b4972c840ff9aec6e50068aec9524
David Kleuker <david.kleuker@greenbone.net>
2018-08-02 14:18:59 +0200

    make grub optional. still enabled by default

--- services/grub.nix
+++ services/grub.nix
@@ -1,8 +1,8 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   # Use the GRUB 2 boot loader
-  boot.loader.grub.enable = true;
+  boot.loader.grub.enable = lib.mkDefault true;
   boot.loader.grub.version = 2;
   boot.loader.grub.memtest86.enable = true;
   boot.loader.timeout = 2;
