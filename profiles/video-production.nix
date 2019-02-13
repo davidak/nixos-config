4d7ffb157d9f42ee4ddf8fe1d0dbf609567f282b
davidak <git@davidak.de>
2019-02-13 07:45:34 +0100

    Clean up

--- profiles/video-production.nix
+++ profiles/video-production.nix
@@ -5,11 +5,7 @@
   environment.systemPackages = with pkgs; [
     audacity
     obs-studio
-    # https://github.com/NixOS/nixpkgs/issues/20449
-    #pitivi
-    # partly broken https://github.com/NixOS/nixpkgs/issues/29614
+    pitivi
     kdeApplications.kdenlive
-    # too simple, also broken
-    #openshot-qt
   ];
 }
