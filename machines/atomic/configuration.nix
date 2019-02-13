07cff60e17207954b285722150f3703be6c0b219
davidak <git@davidak.de>
2019-02-13 03:37:54 +0100

    atomic: enable php opcache

--- machines/atomic/configuration.nix
+++ machines/atomic/configuration.nix
@@ -146,17 +146,16 @@ in
       ;memory_limit = 256M
       ;max_execution_time = 60
 
-      # Problems with Caddy https://github.com/mholt/caddy/issues/1204
-      #zend_extension = ${pkgs.php56}/lib/php/extensions/opcache.so
-      #opcache.enable = 0
-      #opcache.memory_consumption = 64
-      #opcache.interned_strings_buffer = 16
-      #opcache.max_accelerated_files = 10000
-      #opcache.max_wasted_percentage = 5
-      #opcache.use_cwd = 1
-      #opcache.validate_timestamps = 1
-      #opcache.revalidate_freq = 2
-      #opcache.fast_shutdown = 1
+      zend_extension = ${pkgs.php}/lib/php/extensions/opcache.so
+      opcache.enable = 1
+      opcache.memory_consumption = 64
+      opcache.interned_strings_buffer = 16
+      opcache.max_accelerated_files = 10000
+      opcache.max_wasted_percentage = 5
+      opcache.use_cwd = 1
+      opcache.validate_timestamps = 1
+      opcache.revalidate_freq = 2
+      opcache.fast_shutdown = 1
     '';
     pools = {
       piwik = {
