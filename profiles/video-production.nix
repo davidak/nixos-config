6f4ccb48e4ad07e6823ae0cb1121150da7747a93
davidak <git@davidak.de>
2019-07-21 19:12:49 +0200

    Remove pitivi
    old, unstable version

--- profiles/video-production.nix
+++ profiles/video-production.nix
@@ -5,7 +5,8 @@
   environment.systemPackages = with pkgs; [
     audacity
     obs-studio
-    pitivi
+    # wait for 1.0 release; use flatpak RC until then
+    #pitivi
     kdeApplications.kdenlive
   ];
 }
