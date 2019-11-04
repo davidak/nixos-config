5780e73897538b99370b1d0b5724ebba418905a4
David Kleuker <david.kleuker@greenbone.net>
2019-11-04 14:49:45 +0100

    clean up

--- packages/python-gvm/default.nix
+++ packages/python-gvm/default.nix
@@ -5,11 +5,6 @@ python3Packages.buildPythonPackage rec {
   version = "1.0.0";
   name = "${pname}-${version}";
 
-#  src = fetchurl {
-#    url = "https://github.com/greenbone/${pname}/archive/v${version}.tar.gz";
-#    sha256 = "0vfwx93sm7r3s1svz3h22w9ns57v83yl57l8fpqj3nr83cv7kb4l";
-#  };
-
   src = python3Packages.fetchPypi {
     inherit pname version;
     sha256 = "c8062e78b91bf71ff57ee91e35ae033eb2b9bc32c29ea67453c67351926ffb89";
