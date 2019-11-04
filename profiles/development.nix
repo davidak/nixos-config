497a0cf24cda4dbf70876b2249e0ed751fb08601
David Kleuker <david.kleuker@greenbone.net>
2019-11-04 14:50:43 +0100

    Add reuse to dev profile

--- profiles/development.nix
+++ profiles/development.nix
@@ -1,9 +1,13 @@
 { config, pkgs, ... }:
 
+let
+  unstable = import <nixos-unstable> {};
+in
 {
   # install packages for software development
   environment.systemPackages = with pkgs; [
     # broken https://github.com/NixOS/nixpkgs/issues/48107
     #pipenv
+    unstable.reuse
   ];
 }
